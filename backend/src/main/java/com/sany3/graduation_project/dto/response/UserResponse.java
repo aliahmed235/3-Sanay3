cc7a2aa1d311210fe6ed008a64d969ceebfe2bf8
aliahmed235 <aliahmed1174@outlook.com>
2026-04-05 20:12:44 +0200

    Login/Register for Customer/Service provider endpoint

---
+++ backend/src/main/java/com/sany3/graduation_project/dto/response/UserResponse.java
@@ -0,0 +1,24 @@
+package com.sany3.graduation_project.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class UserResponse {
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private Set<String> roles;
+    private Boolean isActive;
+    private String profileImage;
+}
