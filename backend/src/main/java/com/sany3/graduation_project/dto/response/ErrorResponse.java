c3d43b347126530c701ebe9e18c829ea520e85cc
aliahmed235 <aliahmed1174@outlook.com>
2026-04-05 20:09:53 +0200

    Login/Register for Customer/Service provider endpoint

---
+++ backend/src/main/java/com/sany3/graduation_project/dto/response/ErrorResponse.java
@@ -0,0 +1,27 @@
+package com.sany3.graduation_project.dto.response;
+
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ErrorResponse {
+    private String message;
+    private int status;
+    private LocalDateTime timestamp;
+
+    public ErrorResponse(String message, int status) {
+        this.message = message;
+        this.status = status;
+        this.timestamp = LocalDateTime.now();
+    }
+}
