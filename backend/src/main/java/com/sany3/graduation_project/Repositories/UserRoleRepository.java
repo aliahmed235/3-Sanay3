d847ac9ce499031fddcc4d79336acf3fc7a517c9
aliahmed235 <aliahmed1174@outlook.com>
2026-04-25 04:41:11 +0300

    new repositions

--- backend/src/main/java/com/sany3/graduation_project/Repositories/UserRoleRepository.java
+++ backend/src/main/java/com/sany3/graduation_project/Repositories/UserRoleRepository.java
@@ -4,9 +4,10 @@ import com.sany3.graduation_project.entites.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
+/**
+ * Repository for UserRole entity (Many-to-Many mapping)
+ */
 @Repository
 public interface UserRoleRepository extends JpaRepository<UserRole, Long> {
-    List<UserRole> findByUser_Id(Long userId);
+
 }
