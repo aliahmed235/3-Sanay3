8b9d9e98c9dad2afb2abf11a13fbb14e140d8b58
aliahmed235 <aliahmed1174@outlook.com>
2026-04-28 22:41:18 +0300

    phase 2

--- backend/src/main/java/com/sany3/graduation_project/GraduationProjectApplication.java
+++ backend/src/main/java/com/sany3/graduation_project/GraduationProjectApplication.java
@@ -2,15 +2,10 @@ package com.sany3.graduation_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "com.sany3.graduation_project.Repositories")
 public class GraduationProjectApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(GraduationProjectApplication.class, args);
     }
-
 }
