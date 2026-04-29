f0878900e67b21a59e20c64ae3ae20e1c8a771fd
aliahmed235 <aliahmed1174@outlook.com>
2026-04-30 00:40:25 +0300

    phase 2

--- backend/src/main/java/com/sany3/graduation_project/GraduationProjectApplication.java
+++ backend/src/main/java/com/sany3/graduation_project/GraduationProjectApplication.java
@@ -2,8 +2,12 @@ package com.sany3.graduation_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class GraduationProjectApplication {
     public static void main(String[] args) {
         SpringApplication.run(GraduationProjectApplication.class, args);
