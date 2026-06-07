ee9c60ae4a3c5ce385c1b98e1d47804f88d362ed
aliahmed235 <aliahmed1174@outlook.com>
2026-06-07 22:42:59 +0300

    Adding extension feature + editing user information

---
+++ backend/src/main/java/com/sany3/graduation_project/dto/request/UpdateNameRequest.java
@@ -0,0 +1,17 @@
+package com.sany3.graduation_project.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateNameRequest {
+
+    @NotBlank(message = "Name is required")
+    @Size(min = 3, max = 100, message = "Name must be between 3 and 100 characters")
+    private String name;
+}
