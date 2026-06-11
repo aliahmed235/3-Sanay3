2d3013720c218ee41c814637b4cd99d4fa62255f
aliahmed235 <aliahmed1174@outlook.com>
2026-06-11 09:27:36 +0300

    Adding Customer Support feature , Fixing schedule , adding later requests  + Payments and Wallets

---
+++ backend/src/main/java/com/sany3/graduation_project/Repositories/WalletTransactionRepository.java
@@ -0,0 +1,13 @@
+package com.sany3.graduation_project.Repositories;
+
+import com.sany3.graduation_project.entites.WalletTransaction;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WalletTransactionRepository extends JpaRepository<WalletTransaction, Long> {
+
+    Page<WalletTransaction> findByWalletIdOrderByCreatedAtDesc(Long walletId, Pageable pageable);
+}
